ddab71a5f40def11a2917d27d3697d70aecc0229
adnguy3n <nguyeant@mail.gvsu.edu>
2025-04-02 23:11:24 -0400

    The openFile function now hashes the file and appends it to an array of nodes.

--- merkletree.go
+++ merkletree.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 /*
- * Mekle Tree node.
+ * Merkle Tree node.
  */
-/*
 type node struct {
 	hash  []byte
 	left  *node
 	right *node
-}*/
+}
 
 /*
  * Takes file pathnames as command line arguments and computes their top hash.
@@ -22,16 +23,16 @@ type node struct {
 func main() {
 	fmt.Println(os.Args[1:])
 
-	files := openFile(os.Args[1:], nil)
-	fmt.Println(files)
+	nodes := openFile(os.Args[1:], nil)
+	fmt.Println(nodes)
 }
 
 /*
- * Opens the files using the pathnames.
+ * Opens the files using the pathnames and hashes them.
  */
-func openFile(args []string, files []*os.File) []*os.File {
+func openFile(args []string, nodes []node) []node {
 	if len(args) == 0 {
-		return files
+		return nodes
 	}
 
 	file, err := os.Open(os.Args[0])
@@ -39,9 +40,14 @@ func openFile(args []string, files []*os.File) []*os.File {
 		log.Fatal(err)
 	}
 
-	files = append(files, file)
+	defer file.Close()
 
-	file.Close()
+	fileHash := sha1.New()
+	if _, err := io.Copy(fileHash, file); err != nil {
+		log.Fatal(err)
+	}
 
-	return openFile(args[1:], files)
+	nodes = append(nodes, node{hash: fileHash.Sum(nil)})
+
+	return openFile(args[1:], nodes)
 }
